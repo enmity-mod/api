cde4310bbe7ebbcd6beaea09df3ccaba64cd2f24
Zoey <zoey@panties.moe>
2022-03-20 23:12:45 -0400

    new build

--- dist/users.js
+++ dist/users.js
@@ -1,21 +1,30 @@
 export async function fetchCurrentUser() {
     return new Promise((resolve, reject) => {
-        window.enmity.users.fetchCurrentUser().then((user) => {
+        window.enmity.users
+            .fetchCurrentUser()
+            .then((user) => {
             resolve(user);
-        }).catch(reject);
+        })
+            .catch(reject);
     });
 }
 export async function fetchProfile(userID) {
     return new Promise((resolve, reject) => {
-        window.enmity.users.fetchProfile(userID).then((user) => {
+        window.enmity.users
+            .fetchProfile(userID)
+            .then((user) => {
             resolve(user);
-        }).catch(reject);
+        })
+            .catch(reject);
     });
 }
 export async function getUser(userID) {
     return new Promise((resolve, reject) => {
-        window.enmity.users.getUser(userID).then((user) => {
+        window.enmity.users
+            .getUser(userID)
+            .then((user) => {
             resolve(user);
-        }).catch(reject);
+        })
+            .catch(reject);
     });
 }
