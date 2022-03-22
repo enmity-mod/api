3b73496aeec935c6e19813ebb2ee4fe0e4777b9a
Zoey <zoey@panties.moe>
2022-03-21 23:02:53 -0400

    add react api wrapper

---
+++ lib/react.ts
@@ -0,0 +1,239 @@
+const react = window.enmity.react;
+
+/**
+ * The react module itself.
+ *
+ * You need to import this into your code if you implement custom UIs with React.
+ */
+export const React = react.React;
+
+/**
+ * React Button
+ * @see {@link https://reactnative.dev/docs/button}
+ */
+export const Button = react.Button;
+
+/**
+ * React FlatList
+ * @see {@link https://reactnative.dev/docs/flatlist}
+ */
+export const FlatList = react.FlatList;
+
+/**
+ * React Image
+ * @see {@link https://reactnative.dev/docs/image}
+ */
+export const Image = react.Image;
+
+/**
+ * React ImageBackground
+ * @see {@link https://reactnative.dev/docs/imagebackground}
+ */
+export const ImageBackground = react.ImageBackground;
+
+/**
+ * React KeyboardAvoidingView
+ * @see {@link https://reactnative.dev/docs/keyboardavoidingview}
+ */
+export const KeyboardAvoidingView = react.KeyboardAvoidingView;
+
+/**
+ * React Modal
+ * @see {@link https://reactnative.dev/docs/modal}
+ */
+export const Modal = react.Modal;
+
+/**
+ * React Pressable
+ * @see {@link https://reactnative.dev/docs/pressable}
+ */
+export const Pressable = react.Pressable;
+
+/**
+ * React RefreshControl
+ * @see {@link https://reactnative.dev/docs/pressable}
+ */
+export const RefreshControl = react.RefreshControl;
+
+/**
+ * React ScrollView
+ * @see {@link https://reactnative.dev/docs/scrollview}
+ */
+export const ScrollView = react.ScrollView;
+
+/**
+ * React SectionList
+ * @see {@link https://reactnative.dev/docs/sectionlist}
+ */
+export const SectionList = react.SectionList;
+
+/**
+ * React StatusBar
+ * @see {@link https://reactnative.dev/docs/statusbar}
+ */
+export const StatusBar = react.StatusBar;
+
+/**
+ * React Stylesheet
+ * @see {@link https://reactnative.dev/docs/stylesheet}
+ */
+export const StyleSheet = react.StyleSheet;
+
+/**
+ * React Switch
+ * @see {@link https://reactnative.dev/docs/switch}
+ */
+export const Switch = react.Switch;
+
+/**
+ * React Text
+ * @see {@link https://reactnative.dev/docs/text}
+ */
+export const Text = react.Text;
+
+/**
+ * React TextInput
+ * @see {@link https://reactnative.dev/docs/textinput}
+ */
+export const TextInput = react.TextInput;
+
+/**
+ * React TouchableHighlight
+ * @see {@link https://reactnative.dev/docs/TouchableHighlight}
+ */
+export const TouchableHighlight = react.TouchableHighlight;
+
+/**
+ * React TouchableOpacity
+ * @see {@link https://reactnative.dev/docs/TouchableOpacity}
+ */
+export const TouchableOpacity = react.TouchableOpacity;
+
+/**
+ * React TouchableWithoutFeedback
+ * @see {@link https://reactnative.dev/docs/TouchableWithoutFeedback}
+ */
+export const TouchableWithoutFeedback = react.TouchableWithoutFeedback;
+
+/**
+ * React Touchable
+ * @see {@link https://reactnative.dev/docs/Touchable}
+ */
+export const Touchable = react.Touchable;
+
+/**
+ * React View
+ * @see {@link https://reactnative.dev/docs/View}
+ */
+export const View = react.View;
+
+/**
+ * React VirtualizedList
+ * @see {@link https://reactnative.dev/docs/VirtualizedList}
+ */
+export const VirtualizedList = react.VirtualizedList;
+
+/**
+ * Discord Form
+ */
+export const Form = react.Form;
+
+/**
+ * Discord FormArrow
+ */
+export const FormArrow = react.FormArrow;
+
+/**
+ * Discord FormCTA
+ */
+export const FormCTA = react.FormCTA;
+
+/**
+ * Discord FormCTAButton
+ */
+export const FormCTAButton = react.FormCTAButton;
+
+/**
+ * Discord FormCardSection
+ */
+export const FormCardSection = react.FormCardSection;
+
+/**
+ * Discord FormCheckbox
+ */
+export const FormCheckbox = react.FormCheckbox;
+
+/**
+ * Discord FormDivider
+ */
+export const FormDivider = react.FormDivider;
+
+/**
+ * Discord FormHint
+ */
+export const FormHint = react.FormHint;
+
+/**
+ * Discord FormIcon
+ */
+export const FormIcon = react.FormIcon;
+
+/**
+ * Discord FormInput
+ */
+export const FormInput = react.FormInput;
+
+/**
+ * Discord FormLabel
+ */
+export const FormLabel = react.FormLabel;
+
+/**
+ * Discord FormRadio
+ */
+export const FormRadio = react.FormRadio;
+
+/**
+ * Discord FormRow
+ */
+export const FormRow = react.FormRow;
+
+/**
+ * Discord FormSection
+ */
+export const FormSection = react.FormSection;
+
+/**
+ * Discord FormSelect
+ */
+export const FormSelect = react.FormSelect;
+
+/**
+ * Discord FormSubLabel
+ */
+export const FormSubLabel = react.FormSubLabel;
+
+/**
+ * Discord FormSwitch
+ */
+export const FormSwitch = react.FormSwitch;
+
+/**
+ * Discord FormTernaryCheckBox
+ */
+export const FormTernaryCheckBox = react.FormTernaryCheckBox;
+
+/**
+ * Discord FormText
+ */
+export const FormText = react.FormText;
+
+/**
+ * Discord FormTextColors
+ */
+export const FormTextColors = react.FormTextColors;
+
+/**
+ * Discord FormTextSizes
+ */
+export const FormTextSizes = react.FormTextSizes;
