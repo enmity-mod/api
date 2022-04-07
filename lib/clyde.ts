5c7133a27f27fd68863a059e5a3e9f97e1f1d3db
Zoey <zoey@panties.moe>
2022-04-07 09:06:54 -0400

    clyde improvements

--- lib/clyde.ts
+++ lib/clyde.ts
@@ -2,7 +2,9 @@
  * Send a reply as Clyde.
  * @param {string} channelID ID of the channel to reply in
  * @param {string} content Content of the message
+ * @param {string} username Shown username of the message
+ * @param {string} avatarURL Avatar used for the message
  */
-export function sendReply(channelID: string, content: string): void {
-  window.enmity.clyde.sendReply(channelID, content);
+export function sendReply(channelID: string, content: (string | object), username?: string, avatarURL?: string): void {
+  window.enmity.clyde.sendReply(channelID, content, username, avatarURL);
 }
