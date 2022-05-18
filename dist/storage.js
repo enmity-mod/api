d253c5cecbb26623b2a6ece7b6e115066595f25d
FifiTheBulldog <fifithebulldog@outlook.com>
2022-05-18 02:05:08 -0400

    Build again

--- dist/storage.js
+++ dist/storage.js
@@ -2,7 +2,7 @@ export async function getItem(name) {
     return await window.enmity.storage.getitem(name);
 }
 export async function setItem(name, value) {
-    return await window.enmity.storage.setitem(name, value);
+    return await window.enmity.storage.setItem(name, value);
 }
 export async function removeItem(name) {
     await window.enmity.storage.removeItem(name);
