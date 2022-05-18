de2717d34e0864da2f2d27687156551049d6ed86
FifiTheBulldog <fifithebulldog@outlook.com>
2022-05-18 02:04:49 -0400

    Capitalization in storage > setItem

--- lib/storage.ts
+++ lib/storage.ts
@@ -14,7 +14,7 @@ export async function getItem(name: string): Promise<string | null> {
  * @returns {string | null}
  */
 export async function setItem(name: string, value: string): Promise<string | null> {
-  return await window.enmity.storage.setitem(name, value);
+  return await window.enmity.storage.setItem(name, value);
 }
 
 /**
